95836fa116e77380bac002004fe1c63b7b3c32ed
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-07-20 03:26:41 +0530

    Tidy up

--- src/components/hero-section/hero-section.components.jsx
+++ src/components/hero-section/hero-section.components.jsx
@@ -8,9 +8,9 @@ const HeroSection = () => {
     const pageState = null;
 
     const fetchData = async () => {
-        const response = await fetch("/.netlify/functions/getMovies", {
-            method: "POST",
-            body: JSON.stringify({ genre: "Sci-Fi", pageState: pageState }),
+        const response = await fetch('/.netlify/functions/getMovies', {
+            method: 'POST',
+            body: JSON.stringify({ genre: 'Sci-Fi', pageState: pageState }),
         });
         const responseBody = await response.json();
         const movies = responseBody.data.movies_by_genre.values;
@@ -27,22 +27,22 @@ const HeroSection = () => {
                 movie && (
                     <div className='hero-container'>
                         <video muted autoPlay={ true } loop>
-                            <source src={ movie.thumbnail } type="video/mp4" />
+                            <source src={ movie.thumbnail } type='video/mp4' />
                         </video>
                         <div className='info-section-container'>
                         <div className='info-section'>
                             <h2>{ movie.title }</h2>
                             <span>{ movie.synopsis }</span>
-                            <div className="button-section">
-                                <div className="button play">
+                            <div className='button-section'>
+                                <div className='button play'>
                                     <span>
-                                        <i className="fas fa-play"></i>
+                                        <i className='fas fa-play'></i>
                                     </span>
                                     Play
                                 </div>
-                                <div className="button more">
+                                <div className='button more'>
                                     <span>
-                                        <i className="fas fa-info-circle"></i>
+                                        <i className='fas fa-info-circle'></i>
                                     </span>
                                     More info
                                 </div>
