78a82fa92e748b943b719b3275621c67d2be7bfc
Gurmehar Singh Bakshi <greaser.bakshi@gmail.com>
2021-07-21 09:56:16 +0530

    Tidy Up and Render Fix

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
@@ -6,13 +6,38 @@ import Header from './components/header/header.component';
 import HeroSection from './components/hero-section/hero-section.components';
 import GenresList from './components/genres-list/genres-list.components';
 
+const App = () => {
 
-const App = () => (
-  <div className='App'>
-    <Header />
-    <HeroSection />
-    <GenresList />
-  </div>
-);
+  const genreIncrement = 4;
+
+  const [genres, setGenres] = useState([]);
+  const [limit, setLimit] = useState(genreIncrement);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('/.netlify/functions/getGenres', {
+        method: "POST",
+        body: limit,
+      })
+      const responseBody = await response.json();
+      setGenres(responseBody.data.reference_list.values);
+    })();
+  }, [limit]);
+
+  const onPageEnd = () => setLimit(limit + genreIncrement);
+  
+
+  return (
+    <div className='App'>
+      <Header />
+      <HeroSection />
+      <GenresList
+        genres={ genres }
+        limit={ limit }
+        onPageEnd={ onPageEnd }
+      />
+    </div>
+  );
+};
 
 export default App;
